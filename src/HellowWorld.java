6e82462a86c9ad1871fa6e6333e284db7dace578
Echae0 <edujcm01@gmail.com>
2025-04-23 10:52:18 +0900

    java project

---
+++ src/HellowWorld.java
@@ -0,0 +1,12 @@
+
+/*
+ * javac HelloWorld.java
+ * java HelloWorld
+ * Run 단축키: ctrl + f11
+ * ctrl + space 자동완성
+ */
+public class HellowWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!");
+	}
+}
