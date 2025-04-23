9446ff4d517b17c8d54a9b348222874bb9a8ad3d
Echae0 <edujcm01@gmail.com>
2025-04-23 11:22:02 +0900

    import

--- src/workshop/account/test/AccountTest.java
+++ src/workshop/account/test/AccountTest.java
@@ -1,7 +1,11 @@
 package workshop.account.test;
 
+//ctrl + shift + o 오류 있을 땐 잘 작동 안 함.
+import workshop.account.entity.Account;
+
 public class AccountTest {
 	public static void main(String[] args) {
-		
+		//Account 객체
+		Account account = new Account();
 	}
 }
